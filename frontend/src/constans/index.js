8235091bded6e45528e20ab148dbb291eb756fb2
Luis Salazar <l_salazar85@icloud.com>
2021-02-06 00:29:49 -0300

    Update url boxname

--- frontend/src/constans/index.js
+++ frontend/src/constans/index.js
@@ -1,3 +1,3 @@
-const MY_PHOTO = 'https://scontent.faep5-1.fna.fbcdn.net/v/t1.0-9/118404240_10157203921491816_5452134641605056529_n.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=tI-vhSkjeF0AX_tKJXb&_nc_ht=scontent.faep5-1.fna&oh=4e8989d9438fdae77e6f7a3de2bd5a26&oe=5FBD54CD';
+const MY_PHOTO = 'https://scontent.faep4-1.fna.fbcdn.net/v/t1.0-9/118404240_10157203921491816_5452134641605056529_n.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=YOJEOCJ3ZqUAX91suA2&_nc_ht=scontent.faep4-1.fna&oh=5a2c7ab5853c092d1964dbb100c43664&oe=6043CDCD';
 
 export { MY_PHOTO }
