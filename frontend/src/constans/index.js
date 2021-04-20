bdfb987701c0e5aaa4046d12df235234cca700a4
Luis Salazar <l_salazar85@icloud.com>
2021-04-20 19:09:40 -0300

    Add node gyp and photo url

--- frontend/src/constans/index.js
+++ frontend/src/constans/index.js
@@ -1,3 +1,3 @@
-const MY_PHOTO = 'https://scontent.faep4-1.fna.fbcdn.net/v/t1.0-9/118404240_10157203921491816_5452134641605056529_n.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=YOJEOCJ3ZqUAX91suA2&_nc_ht=scontent.faep4-1.fna&oh=5a2c7ab5853c092d1964dbb100c43664&oe=6043CDCD';
+const MY_PHOTO = 'https://scontent.faep33-1.fna.fbcdn.net/v/t1.6435-9/170810186_10157729943471816_8716995957618713047_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=bk3yltZXhawAX_TZpwc&_nc_oc=AQnTLSgyp9e2SaB_qtrOUtHJmOauf1den9aC6EiDHvI9eSiPMlNYtTD96zUMAkLpfFN_zohwTTcA5YJq4PmHqOO8&_nc_ht=scontent.faep33-1.fna&oh=84d48ceeb56e34e2c7fb8e39c99d63e1&oe=60A35B5D';
 
 export { MY_PHOTO }
